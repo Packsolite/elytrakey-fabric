63c7f4b1826b66ee4eb1b29046d798c4f0fcb2be
Packsolite <packsolite@gmail.com>
2024-07-26 12:38:04 +0200

    :fire: Remove unused

--- remappedSrc/de/liquiddev/elytrakey/options/ConfigModel.java
+++
@@ -1,8 +0,0 @@
-package de.liquiddev.elytrakey.options;
-
-public class ConfigModel {
-	public boolean autoEquipFall = true;
-	public boolean autoEquipFirework = false;
-	public boolean autoUnequip = true;
-	public boolean easyTakeoff = true;
-}
