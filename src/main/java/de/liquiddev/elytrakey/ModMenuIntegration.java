8077df6b3df6c353d0c6891ad23a462c1de0fee2
PINPAL <18197217+PINPAL@users.noreply.github.com>
2025-01-21 20:00:35 +0000

    :sparkles: Add ModMenu Integration

---
+++ src/main/java/de/liquiddev/elytrakey/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package de.liquiddev.elytrakey;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import de.liquiddev.elytrakey.ui.ElytraKeyOptions;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> new ElytraKeyOptions();
+    }
+}
