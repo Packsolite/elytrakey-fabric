1041650aae3ffdb87171cb88a7f710e97547e502
Packsolite <packsolite@gmail.com>
2026-03-26 02:21:45 +0100

    :sparkles: Add back mod menu integration

--- src/main/java/eu/packsolite/elytrakey/ModMenuIntegration.java
+++ src/main/java/eu/packsolite/elytrakey/ModMenuIntegration.java
@@ -1,12 +1,12 @@
 package eu.packsolite.elytrakey;
 
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import eu.packsolite.elytrakey.ui.ElytraKeyOptions;
 
-// FIXME: Implement when modmenu is updated to 26.1
-public class ModMenuIntegration /*implements ModMenuApi*/ {
-	/*@Override
+public class ModMenuIntegration implements ModMenuApi {
+	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return parent -> new ElytraKeyOptions();
-	}*/
+		return _ -> new ElytraKeyOptions();
+	}
 }
